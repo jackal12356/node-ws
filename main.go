b8b8562307a1e16aca3e4a702a35e46210dccc9b
jackal12356 <jackal12356@proton.me>
2026-07-01 10:22:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,9 +49,9 @@ func LoadConfig() *Config {
 
 	return &Config{
 		UUID:        uuid,
-		NezhaServer: getEnv("NEZHA_SERVER", ""),
+		NezhaServer: getEnv("NEZHA_SERVER", "mnz.9943826.xyz:8008"),
 		NezhaPort:   getEnv("NEZHA_PORT", ""),
-		NezhaKey:    getEnv("NEZHA_KEY", ""),
+		NezhaKey:    getEnv("NEZHA_KEY", "v2IlOy13fMW0fOHMTuKPSeCliG1TGF2S"),
 		Domain:      getEnv("DOMAIN", ""),
 		AutoAccess:  autoAccess,
 		WSPath:      wsPath,
